973f4ddbb43d8c86eb82d7782b7e91e9cfb0658b
Vibhor Singh <thisbevibhor@gmail.com>
2023-05-22 23:34:31 +0530

    sass

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,28 @@ function App() {
 			title: "Hats",
 			id: 1,
 		},
+		{
+			title: "Jackets",
+			id: 2,
+		},
+		{
+			title: "Sneakers",
+			id: 3,
+		},
+		{
+			title: "Men",
+			id: 4,
+		},
+		{
+			title: "Women",
+			id: 5,
+		},
 	];
 	return (
 		<>
 			<div className="categories-container">
 				{categories.map(({ title }) => (
-					<div className="category-container">
+					<div className="category-container" key={categories.id}>
 						{/* { <img /> } */}
 						<div className="category-body-container">
 							<h2>{title}</h2>
